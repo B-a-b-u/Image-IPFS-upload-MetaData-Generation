a1ddd8814cb49723fef9cedcb36f7df24f7baac9
code:babu <92378964+B-a-b-u@users.noreply.github.com>
2023-10-07 20:54:31 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-// import { useState } from 'react';
 import ImgUpload from './ImgUpload';
 import { BrowserRouter as Router, Route, Routes , Link, BrowserRouter} from 'react-router-dom';
 import ViewImage from './ViewImage';
-// import { Web3Storage } from 'web3.storage';
-// import { upload } from '@testing-library/user-event/dist/upload';
 function App() {
 
 
@@ -12,10 +9,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcome to Image Upload on IPFS</h1>
-        {/* <ImgUpload/> */}
       </header>
       <BrowserRouter>
-      {/* <ImgUpload/> */}
       <Routes>
       <Route path='/' element = {<ImgUpload/>}/>
         <Route path='/view/:cid/:filename' element = {<ViewImage/>}/>
